165247c6fd55698d808693a050f34608cc3da378
조경진 <cho881020@gmail.com>
2021-07-10 15:06:38 +0900

    전달받은 메세지를 찾아내서 텍스트뷰에 반영

--- app/src/main/java/com/neppplus/intent_20210710/ViewMessageActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210710/ViewMessageActivity.kt
@@ -2,10 +2,16 @@ package com.neppplus.intent_20210710
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_message.*
 
 class ViewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_message)
+
+        val receivedMessage = intent.getStringExtra("message")
+
+        messageTxt.text = receivedMessage
+
     }
 }
